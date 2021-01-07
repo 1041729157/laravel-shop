92fe20a4b155f6a8989eef1c1b57e35d8df07bf2
vagrant <vagrant@homestead>
2021-01-07 13:41:04 +0000

    基础布局

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+  <div class="container">
+  	<a class="navbar-brand" href="{{ url('/') }}">
+  	  Laravel Shop
+  	</a>
+  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  	  <span class="navbar-toggler-icon"></span>
+  	</button>
+
+  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+  	  <!-- 左边导航栏 -->
+  	  <ul class="navbar-nav mr-auto">
+  	  	
+  	  </ul>
+
+  	  <!-- 右边导航栏 -->
+  	  <ul class="navbar-nav navbar-right">
+  	  	<li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+  	  	<li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+  	  </ul>
+  	</div>
+
+  </div>
+</nav>
